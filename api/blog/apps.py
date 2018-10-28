42b485b2ffefaa87945a8d9831782b8f79ce59cf
SamTheNormalOne <samscheding@gmail.com>
2018-10-28 19:34:12 +1100

    Initial Commit. Basic Blogging API with Django and Django Rest Framework. Complete with API Docs

---
+++ api/blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    name = 'api.blog'
+    label = 'blog_api'
---
+++ api/blog/serializers.py
@@ -0,0 +1,40 @@
+import pickle
+from rest_framework import serializers
+from apps.blog.models import Article, Tag
+
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class TagsField(serializers.CharField):
+
+        def to_representation(self, tags):
+            tags = tags.all()
+            return "".join([(tag.name + " ") for tag in tags]).rstrip(' ')
+
+    tags = TagsField()
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+    def create(self, validated_data):
+
+        tags = validated_data.pop('tags') # Removes the 'tags' entry
+        tag_list = []
+        for tag in tags.split(' '):
+            tag_instance, created = Tag.objects.get_or_create(name=tag)
+            tag_list += [tag_instance]
+
+        article = Article.objects.create(**validated_data)
+        print(tag_list)
+        article.tags.set(tag_list)
+        article.save()
+        return article
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
