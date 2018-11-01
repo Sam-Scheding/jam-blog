684b42ed140f759a432bd59299547c52ce726620
Jae-Hee <jae-hee@oneflare.com>
2018-11-01 14:57:28 +1100

    add a bit more styling

--- apps/main/src/components/HomePage/Homepage.js
+++ apps/main/src/components/HomePage/Homepage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import key from "weak-key";
 import Nav from '../Nav/nav.js'
-import { TitleStyled } from './HomepageStyled';
+import { TitleStyled, PageStyled, ImgStyled } from './HomepageStyled';
 const Homepage = () =>
   (
-    <div>
+    <PageStyled>
       <Nav />
-      <TitleStyled>This Shit is Bananas</TitleStyled>
-    </div>
+      <TitleStyled>Hello, we are <span>JAM</span></TitleStyled>
+      <ImgStyled><img src="http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/56/cj/p056cj5w.jpg"/></ImgStyled>
+    </PageStyled>
   );
 
 export default Homepage
--- apps/main/src/components/HomePage/HomepageStyled.js
+++ apps/main/src/components/HomePage/HomepageStyled.js
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
 
 export const TitleStyled = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+  font-size: 3.125rem;
+  line-height: 68px;
+  text-align: left;
+  color: black;
+  width: 40%;
+  vertical-align: top;
+  margin: 0;
+  display: inline-block;
+  span {
+    color: #ef4b6c;
+  }
+`;
+
+export const PageStyled = styled.div`
+  padding: 0 20px;
+`;
+
+export const ImgStyled = styled.div`
+  width: 60%
+  display: inline-block;
+  img {
+    width: 100%;
+  }
 `;
